3491ca9beb4e8ddc3c57cca888567d785c913e0b
Boyan Pironkov <bpiro@softserveinc.com>
2022-01-07 15:12:07 +0200

    add tab

--- LetterOccurences/script.js
+++ LetterOccurences/script.js
@@ -1,22 +1,22 @@
 const str = 'szxzshpswspzz';
 
 function evaluateLetterOccurence(strInput) {
-let letters = {};
+	let letters = {};
 
-for (let i = 0; i < strInput.length; i++) {
-	if(!letters[strInput[i]]){
-		letters[strInput[i]] = 1;
+	for (let i = 0; i < strInput.length; i++) {
+		if(!letters[strInput[i]]){
+			letters[strInput[i]] = 1;
+		}
+		else {
+			letters[strInput[i]]++;
+		}
 	}
-	else {
-		letters[strInput[i]]++;
-	}
-}
 
-let maxCount = Math.max(...Object.values(letters));
-let maxCountLetters = Object.keys(letters).filter(key => letters[key] === maxCount);
+	let maxCount = Math.max(...Object.values(letters));
+	let maxCountLetters = Object.keys(letters).filter(key => letters[key] === maxCount);
 
-return (maxCountLetters.length > 1) ? `The letters "${maxCountLetters}" occur maximum number of times: ${maxCount}` 
-                                        : `The letter "${maxCountLetters}" occurs maximum number of times: ${maxCount}`;
+	return (maxCountLetters.length > 1) ? `The letters "${maxCountLetters}" occur maximum number of times: ${maxCount}` 
+											: `The letter "${maxCountLetters}" occurs maximum number of times: ${maxCount}`;
 }
 
 console.log(evaluateLetterOccurence(str));
